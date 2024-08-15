bd0cf708de34e43706b5f19b5a7138212f2ef0ff
demenciabeatsdev <demenciabeats.dev@gmail.com>
2024-08-15 01:03:40 -0400

    add changes

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const testSuiteRoutes = require('./routes/testSuiteRoutes');
+const testSuiteLevel2Routes = require('./routes/testSuiteLevel2Routes'); 
 
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api/test-suites', testSuiteRoutes);
+app.use('/api/test-suites-level-2', testSuiteLevel2Routes);
 
 
 const PORT = process.env.PORT || 3000;
