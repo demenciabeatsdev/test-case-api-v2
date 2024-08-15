d6e3b3a838f333c5da0cd40aece2387762661e70
demenciabeatsdev <demenciabeats.dev@gmail.com>
2024-08-15 13:17:36 -0400

    back complete

--- app.js
+++ app.js
@@ -3,16 +3,20 @@ const app = express();
 const path = require('path');
 const testSuiteRoutes = require('./routes/testSuiteRoutes');
 const testSuiteLevel2Routes = require('./routes/testSuiteLevel2Routes'); 
-
+const actionRoutes = require('./routes/actionRoutes');
+const expectedResultRoutes = require('./routes/expectedResultRoutes');
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api/test-suites', testSuiteRoutes);
+app.use('/api/actions', actionRoutes);
 app.use('/api/test-suites-level-2', testSuiteLevel2Routes);
+app.use('/api/expected-results', expectedResultRoutes);
 
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT} :) `);
 });
+
